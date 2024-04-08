791fb555a61790c2c5da00bb49e0140d96a0084f
Al3xTO <147641630+Al3xTO@users.noreply.github.com>
2024-04-08 22:40:36 +0200

    Add files via upload

---
+++ Stratbuy/Stratbuy/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace Stratbuy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonkitchen = new System.Windows.Forms.Button();
+            this.buttonbathroom = new System.Windows.Forms.Button();
+            this.buttonbedroom = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttoncheck = new System.Windows.Forms.Button();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(54, 115);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(54, 146);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(54, 173);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 8;
+            // 
+            // buttonkitchen
+            // 
+            this.buttonkitchen.Location = new System.Drawing.Point(57, 32);
+            this.buttonkitchen.Name = "buttonkitchen";
+            this.buttonkitchen.Size = new System.Drawing.Size(75, 23);
+            this.buttonkitchen.TabIndex = 9;
+            this.buttonkitchen.Text = "Кухня";
+            this.buttonkitchen.UseVisualStyleBackColor = true;
+            // 
+            // buttonbathroom
+            // 
+            this.buttonbathroom.Location = new System.Drawing.Point(138, 32);
+            this.buttonbathroom.Name = "buttonbathroom";
+            this.buttonbathroom.Size = new System.Drawing.Size(75, 23);
+            this.buttonbathroom.TabIndex = 10;
+            this.buttonbathroom.Text = "Ванна";
+            this.buttonbathroom.UseVisualStyleBackColor = true;
+            // 
+            // buttonbedroom
+            // 
+            this.buttonbedroom.Location = new System.Drawing.Point(219, 32);
+            this.buttonbedroom.Name = "buttonbedroom";
+            this.buttonbedroom.Size = new System.Drawing.Size(75, 23);
+            this.buttonbedroom.TabIndex = 11;
+            this.buttonbedroom.Text = "Спальня";
+            this.buttonbedroom.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(434, 49);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 17;
+            // 
+            // buttoncheck
+            // 
+            this.buttoncheck.Location = new System.Drawing.Point(444, 75);
+            this.buttoncheck.Name = "buttoncheck";
+            this.buttoncheck.Size = new System.Drawing.Size(75, 23);
+            this.buttoncheck.TabIndex = 18;
+            this.buttoncheck.Text = "Вибрати товари";
+            this.buttoncheck.UseVisualStyleBackColor = true;
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "Дешево",
+            "Ціна/Якість",
+            "Дорого"});
+            this.checkedListBox1.Location = new System.Drawing.Point(308, 49);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(120, 49);
+            this.checkedListBox1.TabIndex = 19;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(54, 199);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 20;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(308, 115);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(226, 124);
+            this.textBox2.TabIndex = 21;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(54, 226);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 13);
+            this.label5.TabIndex = 22;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(54, 89);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(51, 13);
+            this.label6.TabIndex = 23;
+            this.label6.Text = "Каталог:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(305, 33);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(69, 13);
+            this.label7.TabIndex = 24;
+            this.label7.Text = "Тип підбору:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(431, 33);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(50, 13);
+            this.label8.TabIndex = 25;
+            this.label8.Text = "Бюджет:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(566, 262);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.buttoncheck);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonbedroom);
+            this.Controls.Add(this.buttonbathroom);
+            this.Controls.Add(this.buttonkitchen);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonkitchen;
+        private System.Windows.Forms.Button buttonbathroom;
+        private System.Windows.Forms.Button buttonbedroom;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttoncheck;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ Stratbuy/Stratbuy/Form1.cs
@@ -0,0 +1,306 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Stratbuy
+{
+    public partial class Form1 : Form
+    {
+        private IStrategy currentStrategy;
+
+        public Form1()
+        {
+            InitializeComponent();
+            buttonkitchen.Click += Buttonkitchen_Click;
+            buttonbathroom.Click += Buttonbathroom_Click;
+            buttonbedroom.Click += Buttonbedroom_Click;
+            checkedListBox1.ItemCheck += CheckedListBox1_ItemCheck;
+            checkedListBox1.SelectionMode = SelectionMode.One;
+            buttoncheck.Click += Buttoncheck_Click;
+        }
+
+        private void Buttoncheck_Click(object sender, EventArgs e)
+        {
+            if (currentStrategy == null)
+            {
+                MessageBox.Show("Виберіть варіант у checkedListBox1", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (decimal.TryParse(textBox1.Text, out decimal budget))
+            {
+                string description = currentStrategy.GetDescription(budget);
+                string[] lines = description.Split('\n');
+                string formattedText = "";
+                decimal totalCost = 0;
+
+                foreach (string line in lines)
+                {
+                    formattedText += $"{line}\r\n";
+
+                    string[] parts = line.Split(',');
+                    if (parts.Length == 3)
+                    {
+                        if (decimal.TryParse(parts[2].Split(':')[1].Trim(), out decimal price))
+                        {
+                            totalCost += price;
+                        }
+                    }
+                }
+
+                decimal remainingBudget = budget - totalCost;
+                formattedText += $"Сума: {totalCost}\r\n";
+                formattedText += $"Залишок: {remainingBudget}";
+                textBox2.Text = formattedText;
+            }
+            else
+            {
+                MessageBox.Show("Введіть числове значення в textBox1", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void CheckedListBox1_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            if (e.NewValue == CheckState.Checked)
+            {
+                for (int i = 0; i < checkedListBox1.Items.Count; i++)
+                {
+                    if (i != e.Index)
+                        checkedListBox1.SetItemChecked(i, false);
+                }
+            }
+            SetStrategy();
+        }
+
+        private void SetStrategy()
+        {
+            switch (checkedListBox1.SelectedIndex)
+            {
+                case 0:
+                    currentStrategy = new CheapStrategy();
+                    break;
+                case 1:
+                    currentStrategy = new PriceQualityStrategy();
+                    break;
+                case 2:
+                    currentStrategy = new ExpensiveStrategy();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void Buttonbedroom_Click(object sender, EventArgs e)
+        {
+            UpdateButtons(Bedroom.Basic, Bedroom.Everest, Bedroom.Nordic, Bedroom.Stella, Bedroom.Joss);
+        }
+
+        private void Buttonbathroom_Click(object sender, EventArgs e)
+        {
+            UpdateButtons(Bathroom.Basic, Bathroom.Cersanit, Bathroom.Acrylic, Bathroom.Dionaplus, Bathroom.Erida);
+        }
+
+        private void Buttonkitchen_Click(object sender, EventArgs e)
+        {
+            UpdateButtons(Kitchen.Basic, Kitchen.Viant, Kitchen.Seven, Kitchen.Millenium, Kitchen.Vintage);
+        }
+
+        private void UpdateButtons(params object[] items)
+        {
+            var labels = new Label[] { label1, label2, label3, label4, label5 };
+
+            for (int i = 0; i < items.Length && i < labels.Length; i++)
+            {
+                var selectedItem = items[i];
+                string name = selectedItem.GetType().GetProperty("Name").GetValue(selectedItem).ToString();
+                string quality = selectedItem.GetType().GetProperty("Quality").GetValue(selectedItem).ToString();
+                string price = selectedItem.GetType().GetProperty("Price").GetValue(selectedItem).ToString();
+
+                labels[i].Text = $"Назва: {name}, Якість: {quality}, Ціна: {price}";
+            }
+        }
+
+        private void UpdateLabels(object selectedItem)
+        {
+            if (selectedItem is Kitchen)
+            {
+                Kitchen kitchen = (Kitchen)selectedItem;
+                label1.Text = $"Назва: {kitchen.Name}, Якість: {kitchen.Quality}, Ціна: {kitchen.Price}";
+            }
+            else if (selectedItem is Bathroom)
+            {
+                Bathroom bathroom = (Bathroom)selectedItem;
+                label2.Text = $"Назва: {bathroom.Name}, Якість: {bathroom.Quality}, Ціна: {bathroom.Price}";
+            }
+            else if (selectedItem is Bedroom)
+            {
+                Bedroom bedroom = (Bedroom)selectedItem;
+                label3.Text = $"Назва: {bedroom.Name}, Якість: {bedroom.Quality}, Ціна: {bedroom.Price}";
+            }
+        }
+
+        public interface IStrategy
+        {
+            string GetDescription(decimal budget);
+        }
+
+        public class CheapStrategy : IStrategy
+        {
+            public string GetDescription(decimal budget)
+            {
+                return $"Кухня: {Kitchen.Basic.Name}, якість: {Kitchen.Basic.Quality}, ціна: {Kitchen.Basic.Price}\n" +
+                       $"Ванна: {Bathroom.Basic.Name}, якість: {Bathroom.Basic.Quality}, ціна: {Bathroom.Basic.Price}\n" +
+                       $"Спальня: {Bedroom.Basic.Name}, якість: {Bedroom.Basic.Quality}, ціна: {Bedroom.Basic.Price}";
+            }
+        }
+
+        public class PriceQualityStrategy : IStrategy
+        {
+            public string GetDescription(decimal budget)
+            {
+                decimal bedroomBudget = budget / 3;
+                decimal kitchenBudget = bedroomBudget;
+                decimal bathroomBudget = budget - (bedroomBudget + kitchenBudget);
+                var availableBathrooms = new List<Bathroom> { Bathroom.Basic, Bathroom.Cersanit, Bathroom.Acrylic, Bathroom.Dionaplus, Bathroom.Erida };
+                Bathroom selectedBathroom = null;
+
+                foreach (var bathroom in availableBathrooms)
+                {
+                    if (bathroom.Price <= bathroomBudget)
+                    {
+                        selectedBathroom = bathroom;
+                    }
+                }
+
+                var availableKitchens = new List<Kitchen> { Kitchen.Basic, Kitchen.Viant, Kitchen.Seven, Kitchen.Millenium, Kitchen.Vintage };
+                Kitchen selectedKitchen = null;
+
+                foreach (var kitchen in availableKitchens)
+                {
+                    if (kitchen.Price <= kitchenBudget)
+                    {
+                        selectedKitchen = kitchen;
+                    }
+                }
+
+                var availableBedrooms = new List<Bedroom> { Bedroom.Basic, Bedroom.Everest, Bedroom.Nordic, Bedroom.Stella, Bedroom.Joss };
+                Bedroom selectedBedroom = null;
+
+                foreach (var bedroom in availableBedrooms)
+                {
+                    if (bedroom.Price <= bedroomBudget)
+                    {
+                        selectedBedroom = bedroom;
+                    }
+                }
+
+                string description = $"Спальня: {selectedBedroom.Name}, якість: {selectedBedroom.Quality}, ціна: {selectedBedroom.Price}\n" +
+                                     $"Кухня: {selectedKitchen.Name}, якість: {selectedKitchen.Quality}, ціна: {selectedKitchen.Price}\n" +
+                                     $"Ванна: {selectedBathroom.Name}, якість: {selectedBathroom.Quality}, ціна: {selectedBathroom.Price}";
+
+                return description;
+            }
+        }
+
+        public class ExpensiveStrategy : IStrategy
+        {
+            public string GetDescription(decimal budget)
+            {
+                decimal bedroomBudget = budget / 3;
+                decimal kitchenBudget = bedroomBudget;
+                decimal bathroomBudget = budget - (bedroomBudget + kitchenBudget);
+
+                var availableBathrooms = new List<Bathroom> { Bathroom.Erida, Bathroom.Dionaplus, Bathroom.Acrylic, Bathroom.Cersanit, Bathroom.Basic };
+                Bathroom selectedBathroom = null;
+
+                foreach (var bathroom in availableBathrooms)
+                {
+                    if (bathroom.Price <= bathroomBudget)
+                    {
+                        selectedBathroom = bathroom;
+                        break;
+                    }
+                }
+
+                var availableKitchens = new List<Kitchen> { Kitchen.Vintage, Kitchen.Millenium, Kitchen.Seven, Kitchen.Viant, Kitchen.Basic };
+                Kitchen selectedKitchen = null;
+
+                foreach (var kitchen in availableKitchens)
+                {
+                    if (kitchen.Price <= kitchenBudget)
+                    {
+                        selectedKitchen = kitchen;
+                        break;
+                    }
+                }
+
+                var selectedBedroom = Bedroom.Stella;
+                string description = $"Спальня: {selectedBedroom.Name}, якість: {selectedBedroom.Quality}, ціна: {selectedBedroom.Price}\n" +
+                                     $"Кухня: {selectedKitchen.Name}, якість: {selectedKitchen.Quality}, ціна: {selectedKitchen.Price}\n" +
+                                     $"Ванна: {selectedBathroom.Name}, якість: {selectedBathroom.Quality}, ціна: {selectedBathroom.Price}";
+
+                return description;
+            }
+        }
+
+        public class Kitchen
+        {
+            public string Name { get; private set; }
+            public int Quality { get; private set; }
+            public decimal Price { get; private set; }
+
+            public Kitchen(string name, int quality, decimal price)
+            {
+                Name = name;
+                Quality = quality;
+                Price = price;
+            }
+
+            public static Kitchen Basic = new Kitchen("Basic", 3, 5000);
+            public static Kitchen Viant = new Kitchen("Viant", 3, 12000);
+            public static Kitchen Seven = new Kitchen("Seven", 4, 12000);
+            public static Kitchen Millenium = new Kitchen("Millenium", 5, 70000);
+            public static Kitchen Vintage = new Kitchen("Vintage", 5, 20000);
+        }
+
+        public class Bathroom
+        {
+            public string Name { get; private set; }
+            public int Quality { get; private set; }
+            public decimal Price { get; private set; }
+
+            public Bathroom(string name, int quality, decimal price)
+            {
+                Name = name;
+                Quality = quality;
+                Price = price;
+            }
+
+            public static Bathroom Basic = new Bathroom("Basic", 3, 8000);
+            public static Bathroom Cersanit = new Bathroom("Cersanit", 4, 15000);
+            public static Bathroom Acrylic = new Bathroom("Acrylic", 4, 60000);
+            public static Bathroom Dionaplus = new Bathroom("Dionaplus", 5, 75000);
+            public static Bathroom Erida = new Bathroom("Erida", 5, 85000);
+        }
+
+        public class Bedroom
+        {
+            public string Name { get; private set; }
+            public int Quality { get; private set; }
+            public decimal Price { get; private set; }
+
+            public Bedroom(string name, int quality, decimal price)
+            {
+                Name = name;
+                Quality = quality;
+                Price = price;
+            }
+
+            public static Bedroom Basic = new Bedroom("Basic", 3, 10000);
+            public static Bedroom Everest = new Bedroom("Everest", 4, 15000);
+            public static Bedroom Nordic = new Bedroom("Nordic", 3, 12000);
+            public static Bedroom Stella = new Bedroom("Stella", 5, 50000);
+            public static Bedroom Joss = new Bedroom("Joss", 5, 80000);
+        }
+    }
+}
